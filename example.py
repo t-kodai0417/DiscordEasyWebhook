812fb6f73f576b73329860386a0e4aefe423c9e3
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-02-08 17:20:44 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import dehook as dk
 webhook=r"https://discord.com/api/webhooks/010101010101010101010101/yajuu_senpai11451444444_0101010101010101-p-_010101010101"
-icon=r"https://discord.com/assets/46b2132c01604c9493d558de444929f4.svg"
+icon=r"https://github.githubassets.com/apple-touch-icon.png"
 #メッセージのみ送信
 dk.normal(webhook,"hello!")
 #名前とアイコンをカスタム
