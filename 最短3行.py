476c495f549a50e290c2ab7beac7ce4d86fdb677
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-02-08 18:05:47 +0900

    Update 最短3行.py

--- 最短3行.py
+++ 最短3行.py
@@ -1,3 +1,3 @@
 import dehook as dk
 webhook=r"https://discord.com/api/webhooks/010101010101010101010101/yajuu_senpai11451444444_0101010101010101-p-_010101010101"
-dk.normal(webhook,"Hello! \n I am Tester!")
+dk.normal(webhook,"Hello!"+'\n'+"I am Tester!")
