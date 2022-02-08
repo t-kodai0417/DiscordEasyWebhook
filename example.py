f2747276d06694f5c68c433a7507dae9ccaec71b
t-Kodai <75510055+t-kodai0417@users.noreply.github.com>
2022-02-08 17:11:53 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,1 +1,11 @@
-import dehook
+import dehook as dk
+webhook=r"https://discord.com/api/webhooks/010101010101010101010101/yajuu_senpai11451444444_0101010101010101-p-_010101010101"
+icon=r"https://discord.com/assets/46b2132c01604c9493d558de444929f4.svg"
+#メッセージのみ送信
+dk.normal(webhook,"hello!")
+#名前とアイコンをカスタム
+dk.user(webhook,"Hello!(2)",icon,"Tester")
+#簡単なEmbed
+dk.easy_embed(webhook,"Hi!","I am Tester.")
+
+
